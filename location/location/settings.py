a2a209c2a226d7ce22b94973a6a113479a90a642
Godfrey-Ndungu <godmak2022@gmail.com>
2023-05-17 16:03:12 +0300

    Add python decouple for environment variable management and update project context

--- location/location/settings.py
+++ location/location/settings.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = ""
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
