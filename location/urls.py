a3997d2c8a487cd90a262f56a480dc966c3a084f
Godfrey-Ndungu <godmak2022@gmail.com>
2023-05-18 14:03:50 +0300

    Add swagger schema url
    Redoc depends on swagger schema

--- location/urls.py
+++ location/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from drf_spectacular.views import SpectacularRedocView
+from django.urls import path, include
+from drf_spectacular.views import SpectacularRedocView, SpectacularAPIView
 
 urlpatterns = [
+    path("", include("api.urls")),
     path("admin/", admin.site.urls),
+    path('api/schema/',
+         SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
