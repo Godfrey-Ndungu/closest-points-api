22641815404ea555bcda6ebfa57b10483345940e
Godfrey-Ndungu <godmak2022@gmail.com>
2023-05-18 16:05:53 +0300

    Add utility class for point manipulation
    This commit include the addition of the utility class ClosestPointFinder to handle point-related operations

--- api/admin.py
+++ api/admin.py
@@ -4,14 +4,14 @@ from .models import ClosestPoint
 
 
 class PointAdmin(admin.ModelAdmin):
-    list_display = ('id', 'created_at', 'points')
-    list_filter = ('created_at',)
-    search_fields = ('points',)
+    list_display = ("id", "created_at", "points")
+    list_filter = ("created_at",)
+    search_fields = ("points",)
 
 
 class ClosestPointAdmin(admin.ModelAdmin):
-    list_display = ('id', 'point', 'closest_points')
-    search_fields = ('points__point', 'closest_points')
+    list_display = ("id", "point", "closest_points")
+    search_fields = ("points__point", "closest_points")
 
 
 admin.site.register(ClosestPoint, ClosestPointAdmin)
