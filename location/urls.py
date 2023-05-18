b53fcf53c44a1823b37a85c78540c6149090cc67
Godfrey-Ndungu <godmak2022@gmail.com>
2023-05-18 11:14:08 +0300

    Integrate Rest Swagger for comprehensive API documentation and interactive exploration

--- location/urls.py
+++ location/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
