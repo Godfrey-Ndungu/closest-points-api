119f96732bc9b2c2cb024601e55d521e56ff9602
Godfrey-Ndungu <godmak2022@gmail.com>
2023-05-18 13:27:51 +0300

    Add default parser and renderers for drf

--- location/settings.py
+++ location/settings.py
@@ -85,6 +85,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
